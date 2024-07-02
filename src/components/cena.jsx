62ee8c2bb2ecf7854ac21054556f7b8bb8b812a9
TediBash <tedi.bashuri@gmail.com>
2024-07-02 10:00:38 +0200

    Add catalogo colazione

--- src/components/cena.jsx
+++ src/components/cena.jsx
@@ -5,7 +5,8 @@ function Cena(){
     return(
         <section>
             <NavHeader />
-            Cena
+            <div className='cenaContainer'>
+            </div>
         </section>
     )
 }
--- src/components/colazione.jsx
+++ src/components/colazione.jsx
@@ -6,25 +6,51 @@ function Colazione(){
         <section>
             <NavHeader />
             <div className='colazioneContainer'>
-                <p><span>Caffe Espresso</span> <span>&#8364; 1.20</span></p>
-                <p><span>Caffe Corretto</span> <span>&#8364; 1.20</span></p>
-                <p><span>Caffe Decaffeinato</span> <span>&#8364; 1.20</span></p>
-                <p><span>Caffe Ginseng</span> <span>&#8364; 1.20</span></p>
-                <p><span>Caffe D'orzo</span> <span>&#8364; 1.20</span></p>
-                <p><span>Caffe Americano</span> <span>&#8364; 1.20</span></p>
-                <p><span>Caffe Macchiato Soia</span> <span>&#8364; 1.20</span></p>
-                <p><span>Caffe Shakerato corretto</span> <span>&#8364; 1.20</span></p>
-                <p><span>Marocchino / Collino</span> <span>&#8364; 1.20</span></p>
-                <p><span>Cioccolata calda</span> <span>&#8364; 1.20</span></p>
-                <p><span>Marocchino con Nutella</span> <span>&#8364; 1.20</span></p>
-                <p><span>Marocchino con crema di pistacchio</span> <span>&#8364; 1.20</span></p>
-                <p><span>Marocchino Dec / Ginseng / Orzo / Soia e chi ne ha piu ne metta</span> <span>&#8364; 1.20</span></p>
-                <p><span>Irish Coffee</span> <span>&#8364; 1.20</span></p>
-                <p><span>Cappuccino</span> <span>&#8364; 1.20</span></p>
-                <p><span>Cuppuccino Dec / Ginseng / Orzo / Soia</span> <span>&#8364; 1.20</span></p>
-                <p><span>Latte macchiato</span> <span>&#8364; 1.20</span></p>
-                <p><span>Latte macchiato di soia</span> <span>&#8364; 1.20</span></p>
-                <p><span>Spremuta</span> <span>&#8364; 1.20</span></p>
+                <p><span>Caffetteria</span> <span></span></p>
+                <p><span>Caff&#233; Espresso</span> <span>&#8364; 1.20</span></p>
+                <p><span>Caff&#233; Corretto</span> <span>&#8364; 1.70</span></p>
+                <p><span>Caff&#233; Decaffeinato</span> <span>&#8364; 1.40</span></p>
+                <p><span>Caff&#233; Ginseng</span> <span>&#8364; 1.60</span></p>
+                <p><span>Caff&#233; D'orzo</span> <span>&#8364; 1.60</span></p>
+                <p><span>Caff&#233; Americano</span> <span>&#8364; 1.60</span></p>
+                <p><span>Caff&#233; Macchiato Soia</span> <span>&#8364; 1.40</span></p>
+                <p><span>Caff&#233; Shakerato</span> <span>&#8364; 3.00</span></p>
+                <p><span>Caff&#233; Shakerato corretto</span> <span>&#8364; 3.50</span></p>
+                <p><span>Marocchino / Collino</span> <span>&#8364; 1.60</span></p>
+                <p><span>Cioccolata calda</span> <span>&#8364; 3.00</span></p>
+                <p><span>Marocchino con Nutella</span> <span>&#8364; 2.00</span></p>
+                <p><span>Marocchino con crema di pistacchio</span> <span>&#8364; 2.50</span></p>
+                <p><span>Marocchino Dec / Ginseng / Orzo / Soia e chi ne ha piu ne metta</span> <span>&#8364; 2.00</span></p>
+                <p><span>Irish Coffee</span> <span>&#8364; 6.00</span></p>
+                <p><span>Cappuccino</span> <span>&#8364; 1.50</span></p>
+                <p><span>Cuppuccino Dec / Ginseng / Orzo / Soia</span> <span>&#8364; 2.00</span></p>
+                <p><span>Latte macchiato</span> <span>&#8364; 2.00</span></p>
+                <p><span>Latte macchiato di soia</span> <span>&#8364; 2.50</span></p>
+                <p><span>Spremuta</span> <span>&#8364; 4.00</span></p>
+                
+                <p className='colazioneDivisore'><span>Croissant Classica vuota</span> <span>&#8364; 1.40</span></p>
+                <p><span>Croissant Classica Farcita</span> <span>&#8364; 1.60</span></p>
+                <p><span>Croissant Farcita salata</span> <span>&#8364; 2.50</span></p>
+                <p><span>Croissant Carbone vegetale</span> <span>&#8364; 1.80</span></p>
+                <p><span>Croissant Vegana Vuota / Farcita</span> <span>&#8364; 1.80</span></p>
+                <p><span>Croissant Intergrale ai cereali</span> <span>&#8364; 1.60</span></p>
+
+                <p className='colazioneDivisore'><span>Treccina pere e Noci</span> <span>&#8364; 1.60</span></p>
+                <p><span>Fagottino nocciole</span> <span>&#8364; 1.60</span></p>
+                <p><span>Pain au chocolate</span> <span>&#8364; 1.60</span></p>
+                <p><span>Fagottino 3 cioccolati</span> <span>&#8364; 1.60</span></p>
+                <p><span>Krapfen</span> <span>&#8364; 1.60</span></p>
+                <p><span>Maritozzo alla panna</span> <span>&#8364; 3.00</span></p>
+                <p><span>Donut funny</span> <span>&#8364; 2.00</span></p>
+                <p><span>Macarons (pz.)</span> <span>&#8364; 2.00</span></p>
+                <p><span>Muffin</span> <span>&#8364; 2.50</span></p>
+
+                <p className='colazioneDivisore'><span>Piadina Farcita</span> <span>&#8364; 3.50</span></p>
+                <p><span>Tramezzino piccolo</span> <span>&#8364; 3.50</span></p>
+                <p><span>Tramezzino grande</span> <span>&#8364; 3.50</span></p>
+                <p><span>Uova e bacon</span> <span>&#8364; 7.00</span></p>
+                <p><span>Panino farcito grande</span> <span>&#8364; 3.50</span></p>
+                <p><span>Panino farcito piccolo</span> <span>&#8364; 2.50</span></p>
             </div>
         </section>
     )
